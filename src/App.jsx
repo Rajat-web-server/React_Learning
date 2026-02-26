f005102923174ff1226fc312b61ce2f87f8aab7d
Your Name <rajatpandey542@gmail.com>
2026-02-26 21:41:20 +0530

    Component with loading and error state

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Appp } from "./UseRef.jsx"
 import {CounterWithInit} from "./counterWithinit.jsx"
 import { CustomCount } from "./CustomCounter.jsx"
 import { MyComponent } from "./myComponent.jsx"
+import { Loading } from "./loading.jsx"
 
 
 function App() {
   return (
     <div>
-      <MyComponent/>
+      <Loading/>
+      {/* <MyComponent/> */}
     </div>
 
   )
---
+++ src/loading.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+export function Loading() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const data = await response.json();
+        setUsers(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchUsers();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+}
